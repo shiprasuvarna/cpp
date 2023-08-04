aa453b7cde1b652711954b944442f3105858325d
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-08-04 23:25:00 +0530

    Add files via upload

---
+++ area_circle.cpp
@@ -0,0 +1,13 @@
+//8. Write a program to calculate area of circle.
+#include<iostream>
+using namespace std;
+main()
+{
+    int r;
+	float area;
+	cout<<"Enter the radius of the circle: "<<endl;
+	cin>>r;
+	area=22/7*r*r;
+	cout<<"The area of the circle is: "<<area;
+}
+
