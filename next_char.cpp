f7345cd731a485a3b8797912f1720998e897d248
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-08-07 21:00:00 +0530

    prints the next character

---
+++ next_char.cpp
@@ -0,0 +1,11 @@
+//Q4 print next character from the character passed
+#include<iostream>
+using namespace std;
+main()
+{
+	char i, j;
+	cout<<"Enter an alphabet: "<<endl;
+	cin>>i;
+	j=++i;
+	cout<<"The next alphabet is: "<<j;
+}
