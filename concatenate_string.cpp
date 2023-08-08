35e6497b04f69b1d0acca3e9746f3c0c9a497496
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-08-08 16:36:55 +0530

    prog to concatenate two strings

---
+++ concatenate_string.cpp
@@ -0,0 +1,12 @@
+//Q24 write a program to concatenate two strings
+#include<iostream>
+using namespace std;
+main()
+{
+	string a,b,c;
+	cout<<"Enter two strings:"<<endl;
+	cin>>a>>b;
+	c=a+b;
+	cout<<"After concatenating: "<<endl<<c<<endl;
+	
+}
