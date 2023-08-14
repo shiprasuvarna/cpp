e8e427ad7f1621b9eaf93a3e23f84ed0e533cc15
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-08-14 16:28:54 +0530

    prog to find area of circle

---
+++ area of the circle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+		float radius, area;
+		 
+		 cout << "enter the radius of the circle:";
+		 cin >> radius;
+		 
+		 area = 3.14*radius*radius;
+		 cout << "enter the radius of the circle:" << area << endl;
+		 
+		 return 0;
+		 
+}
+
