8bf57f07f11181bbd9bccb1c1df22ec40814eccd
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-08-22 21:01:22 +0530

    Add files via upload

---
+++ multiple_arguement_function.cpp
@@ -0,0 +1,11 @@
+//multiple arguement function
+#include<iostream>
+using namespace std; 
+void printNum(int num, float val)   //creating the function
+{
+	cout<<num<<endl<<val;
+}
+main()
+{
+	printNum(19,19.06);  //calling the function
+}
