b4147830bfe462d52b17e6fe009bdbc83290bc08
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-08-22 21:02:21 +0530

    Add files via upload

---
+++ single_arguement_function.cpp
@@ -0,0 +1,11 @@
+//single arguement function
+#include<iostream>
+using namespace std;
+void printNum(int num)   //creating the function printNum and (int num) is the arguement which includes the datatype and variable
+{
+	cout<<num;
+}
+main()
+{
+	printNum(19);
+}
