ff714e154e99a97ff649aec7204e00438c86dce9
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-08-23 18:41:58 +0530

    prog to find whether a no is even or odd

---
+++ even_odd.cpp
@@ -0,0 +1,17 @@
+//10. Write a program to check whether the given number is even or odd 
+#include<iostream>
+using namespace std;
+main()
+{
+	int a;
+	cout<<"Enter a number: "<<endl;
+	cin>>a;
+	if(a%2==0)
+	{
+		cout<<"The number is even"<<endl;
+	}
+	else
+	{
+		cout<<"The number is odd"<<endl;
+	}
+}
