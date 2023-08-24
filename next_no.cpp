cd979df7220a016fec57c3c07c056d203af4b26a
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-08-24 23:01:41 +0530

    prog to print next number

---
+++ next_no.cpp
@@ -0,0 +1,11 @@
+//Q5 prog to find the next number from the integer passed
+#include<iostream>
+using namespace std;
+main()
+{
+	int i, j;
+	cout<<"Enter a number: "<<endl;
+	cin>>i;
+	j=++i;
+	cout<<"The next number is: "<<j;
+}
