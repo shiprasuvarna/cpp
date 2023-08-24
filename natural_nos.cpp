2e3fb9cb02378281b21a4ac94fa603f20e45919e
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-08-24 23:01:03 +0530

    prog to find first 10 natural nos

---
+++ natural_nos.cpp
@@ -0,0 +1,42 @@
+//Q1 prog to find first 10 natural numbers
+//using for loop
+/*
+#include<iostream>
+using namespace std;
+main()
+{
+	int i;
+	for(i=1; i<=10; i++)
+	{
+		cout<<i<<"\t";
+	}
+}
+*/
+
+//using while loop
+#include<iostream>
+using namespace std;
+main()
+{
+	int i=1;
+	while(i<=10)
+	{
+		cout<<i<<"\t";
+		i++;
+	}
+}
+
+
+//do while loop
+/*
+#include<iostream>
+using namespace std;
+main()
+{
+	int i=1;
+	do{
+		cout<<i<<"\t";
+		i++;
+	}while(i<=10);
+}
+*/
